4e58423ff7c08312f37fec3c4407155166d6dd13
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 14:09:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,13 +47,3 @@ def hit?(card_total)
     get_user_input
   end
 end
-
-def runner
-  welcome
-  hand=initial_round
-  until hand>21 do
-    hand=hit?(hand)
-    display_card_total(hand)
-  end
-  end_game(hand)
-end
